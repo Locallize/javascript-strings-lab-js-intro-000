8902758e789a6af071dcfd8dfa9d1b674e29569d
beneficial-kernel-0493 <fillanlee@aol.com>
2019-01-05 01:18:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ greeting = "Hello, everybody"
 var specialGuest = "Neil deGrasse Tyson"
 
 var greetSpecialGuest = "" + specialGuest + "!";
-
+greetSpecialGuest = `Hello, ${specialGuest}!`;
 var topic = "space";
 
 var conversation = `${topic}`;
+conversation = `Let's talk about ${topic}.`;
